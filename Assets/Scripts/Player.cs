b5fcce24169ef99f4767dacffb32b2c004f6534b
VladDuncea <vlad_duncea_31@yahoo.com>
2017-05-13 21:13:04 +0300

    player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+	public float speed=80/36f;
+	void Start () 
+	{
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update () 
+	{
+		Vector3 force = new Vector3 (transform.position.x,transform.position.y,transform.position.z);
+		force.z += speed * Time.deltaTime; 
+		transform.position = force;
 	}
 }
