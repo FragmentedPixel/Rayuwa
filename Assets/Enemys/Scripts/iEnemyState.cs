21cf2d797d1b027d80ec1142885c729f4b75864c
VladDuncea <vlad_duncea_31@yahoo.com>
2017-05-14 01:08:34 +0300

    A* implementation

---
+++ Assets/Enemys/Scripts/iEnemyState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract  class iEnemyState  
+{
+	public EnemyController controller;
+
+	public iEnemyState (EnemyController eController)
+	{
+		controller = eController;
+	}
+
+	public abstract void Update ();
+
+	public abstract void OnTriggerEnter (Collider other);
+
+	public abstract void ToAttack ();
+	public abstract void ToChase ();
+	public abstract void ToPatrol ();
+
+}
