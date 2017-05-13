13ebd2529a9d9a8e54780d93108ce47953370f6f
Oprea Alexandru <oalexprea@yahoo.com>
2017-05-13 22:26:37 +0300

    Main Menu + Splash Screen + Music

---
+++ Assets/Scripts/Autoload.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Autoload : MonoBehaviour
+{
+    public string sceneName;
+
+    public AudioClip introSound;
+    private AudioSource audioS;
+
+    private void Start()
+    {
+        PlayIntroSound();
+        StartCoroutine(LoadSceneCR());
+    }
+
+    private void PlayIntroSound()
+    {
+        audioS = GetComponent<AudioSource>();
+        audioS.PlayOneShot(introSound);
+    }
+
+    private IEnumerator LoadSceneCR()
+    {
+        yield return new WaitForSeconds(introSound.length + .5f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/Scripts/PersistentMusic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentMusic : MonoBehaviour
+{
+    public AudioClip backgroundLoop;
+
+    private AudioSource audioS;
+
+	private void Start ()
+    {
+        StartMusic();
+        DontDestroyOnLoad(this);
+	}
+
+
+    private void StartMusic()
+    {
+        audioS = GetComponent<AudioSource>();
+        audioS.clip = backgroundLoop;
+        audioS.Play();
+    }
+}
