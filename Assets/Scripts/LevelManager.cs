dfe121e22a488f342a982e0215c4120afe54a782
Oprea Alexandru <oalexprea@yahoo.com>
2017-05-13 21:37:11 +0300

    Level Manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public AudioClip buttonSound;
+    private AudioSource audioS;
+
+    private void Start()
+    {
+        audioS = GetComponent<AudioSource>();
+    }
+
+    public void ChangeScene(string scene)
+    {
+        audioS.PlayOneShot(buttonSound);
+        StartCoroutine(ChangeSceneCR(scene));
+    }
+
+    private IEnumerator ChangeSceneCR(string scene)
+    {
+        yield return new WaitForSeconds(buttonSound.length);
+        SceneManager.LoadScene(scene);
+        yield break;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
