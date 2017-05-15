23c2a040a56b14c89cf22618fdbd8f22d4462293
VladDuncea <vlad_duncea_31@yahoo.com>
2017-05-16 00:33:35 +0300

    UnitINstatianteion

--- Assets/Units/Scripts/UnitsManager.cs
+++ Assets/Units/Scripts/UnitsManager.cs
@@ -1,16 +1,40 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitsManager : MonoBehaviour {
+public class UnitsManager : MonoBehaviour
+{
+	public int unitCount=5;
+	public List<Transform> points;
+	public GameObject units;
 
-	// Use this for initialization
-	void Start () {
-		
+	public void Start()
+	{
+		Spawn ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void Spawn()
+    {
+		for (int i = 0; i < points.Count; i++)
+		{
+			Transform temp = points [i];
+			int randomIndex = Random.Range (i, points.Count);
+			points [i] = points [randomIndex];
+			points [randomIndex] = temp;
+		}
+		for(int i =0;i<unitCount;i++)
+		{
+			Instantiate(units,points[i].position,Quaternion.identity,transform);
+		}
+     }
+
+    public void StartBattle()
+    {
+       UnitController[] controllers = FindObjectsOfType<UnitController>();
+
+		foreach(UnitController controller in controllers)
+          	 controller.StartBattle();
+    }
+
+
 }
