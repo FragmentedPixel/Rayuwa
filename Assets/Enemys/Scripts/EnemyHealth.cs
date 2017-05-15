4c8c63329b6d4355d3bdc637c4afee05f098026b
VladDuncea <vlad_duncea_31@yahoo.com>
2017-05-15 22:22:23 +0300

    HealthBar Implementation

--- Assets/Enemys/Scripts/EnemyHealth.cs
+++ Assets/Enemys/Scripts/EnemyHealth.cs
@@ -1,28 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour {
 	public float MaxHealth=500;
 	private float currentHealth;
-	// Use this for initialization
+	public Image background;
+	private Image healthImage;
+	private Canvas canvas;
 	void Start () 
-	{
-		currentHealth = MaxHealth;
+	{	
+		healthImage = background.GetComponentInChildren<Image> ();
+		canvas = background.GetComponentInParent<Canvas>();
+		currentHealth = MaxHealth;	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		canvas.transform.LookAt (Camera.main.transform);
 	}
 
 	public void Hit(float damage)
 	{
 		currentHealth -= damage;
+		healthImage.fillAmount = currentHealth / MaxHealth;
+		if (healthImage.fillAmount < 0.2)
+			healthImage.color = Color.red;
 		if (currentHealth <= 0)
 		{
 			Destroy (transform.parent.gameObject);
 		}
-		Debug.Log (currentHealth);
 	}
 }
--- Assets/Units/Scripts/UnitHealth.cs
+++ Assets/Units/Scripts/UnitHealth.cs
@@ -1,29 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnitHealth : MonoBehaviour {
 
 	public float MaxHealth=500;
 	private float currentHealth;
+	public Image background;
+	private Image healthImage;
+	private Canvas canvas;
 	// Use this for initialization
 	void Start () 
 	{
+		healthImage = background.GetComponentInChildren<Image> ();
+		canvas = background.GetComponentInParent<Canvas>();
+
 		currentHealth = MaxHealth;	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		canvas.transform.LookAt (Camera.main.transform);
 	}
 
 	public void Hit(float damage)
 	{
 		currentHealth -= damage;
+		healthImage.fillAmount = currentHealth / MaxHealth;
+		if (healthImage.fillAmount < 0.2)
+			healthImage.color = Color.red;
 		if (currentHealth <= 0)
 		{
 			Destroy (transform.parent.gameObject);
 		}
-		Debug.Log (currentHealth);
 	}
 }
