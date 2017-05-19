4bd45c7b9b98867e5b39f4e25ca2ad17eb598184
VladDuncea <vlad_duncea_31@yahoo.com>
2017-05-20 01:53:05 +0300

    Terrain height

---
+++ Assets/Enemys/Scripts/Ionut.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ionut : MonoBehaviour {
+    public float groundHeight;
+    public LayerMask walkableMask;
+    public Transform parentTransform;
+    // Use this for initialization
+    void Start()
+    {
+        parentTransform = GetComponentInParent<Transform>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit raycastHit;
+        if (Physics.Raycast(ray, out raycastHit, 15, walkableMask))
+        {
+            Debug.DrawLine(transform.position, raycastHit.point,Color.red);
+            Vector3 desiredTransform = parentTransform.position;
+            desiredTransform.y += -(transform.position.y - raycastHit.point.y) + groundHeight; 
+           transform.parent.position = desiredTransform;
+        }
+    }
+}
