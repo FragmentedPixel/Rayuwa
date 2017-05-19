bc06b45b845a39c93694fb8e23bf11c26d501873
VladDuncea <vlad_duncea_31@yahoo.com>
2017-05-20 00:22:23 +0300

    Camera MouseControll

--- Assets/Generals/Scripts/CameraController.cs
+++ Assets/Generals/Scripts/CameraController.cs
@@ -6,23 +6,29 @@ public class CameraController : MonoBehaviour {
 	public float speed = 10.0F;
 	private Vector3 startPosition;
 	public float Z_Boundary=50f;
-    public int screenWidth;
+    [Range(0,0.5f)]
+    public float screenPercent = 0.1f;
+    private float screenMoveSize;
 	void Start () 
 	{
 		startPosition = transform.position;
+        screenMoveSize = Screen.width* screenPercent;
 	}
 		
 
 
 	void Update() 
 	{
-		float movement = Input.GetAxis("Horizontal") * speed;
-		movement *= Time.deltaTime;
-        if (movement == 0)
-            Debug.Log(Input.mousePosition);
-        movement += transform.position.z;
+		float movement = Input.GetAxis("Horizontal");
 
-        
+        if (movement == 0)
+            if (Input.mousePosition.x < screenMoveSize)
+                movement = -1 * Mathf.InverseLerp(screenMoveSize, 0, Input.mousePosition.x);
+            else if (Input.mousePosition.x > Screen.width - screenMoveSize)
+                movement = 1 * Mathf.InverseLerp(Screen.width - screenMoveSize, Screen.width, Input.mousePosition.x);
+
+        movement *= Time.deltaTime * speed;
+        movement += transform.position.z;
 
 		if(movement>startPosition.z&&movement<startPosition.z+Z_Boundary)
 			transform.position += new Vector3 (0,0,movement-transform.position.z);
