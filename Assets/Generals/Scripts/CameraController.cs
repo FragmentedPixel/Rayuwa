36a917e0daa635ff999198d7a6ce2eb7f02bfb35
VladDuncea <vlad_duncea_31@yahoo.com>
2017-05-22 22:41:13 +0300

    Side Camera Update

--- Assets/Generals/Scripts/CameraController.cs
+++ Assets/Generals/Scripts/CameraController.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-	public float speed = 10.0F;
+	
 	private Vector3 startPosition;
-	public float Z_Boundary=50f;
-    [Range(0,0.5f)]
-    public float screenPercent = 0.1f;
     private float screenMoveSize;
-	void Start () 
+    [Header("Speeds")]
+    public float speed = 10.0F;
+    public float rot_speed = 10.0F;
+    
+    [Header("Boundaries")]
+    [Range(0, 0.5f)]
+    public float screenPercent = 0.1f;
+    public float Z_Boundary = 50f;
+    [Range(0, 360f)]
+    public float Y_Rot_Min = 240f;
+    [Range(0, 360f)]
+    public float Y_Rot_Max = 300f;
+
+    void Start () 
 	{
         screenPercent = PlayerPrefsManager.GetScrollBoundray();
 		startPosition = transform.position;
@@ -21,17 +31,33 @@ public class CameraController : MonoBehaviour {
 	void Update() 
 	{
 		float movement = Input.GetAxis("Horizontal");
+        if (movement != 0)
+        {
+            movement *= Time.deltaTime * speed;
+            movement += transform.position.z;
 
-        if (movement == 0)
-            if (Input.mousePosition.x < screenMoveSize)
-                movement = -1 * Mathf.InverseLerp(screenMoveSize, 0, Input.mousePosition.x);
-            else if (Input.mousePosition.x > Screen.width - screenMoveSize)
-                movement = 1 * Mathf.InverseLerp(Screen.width - screenMoveSize, Screen.width, Input.mousePosition.x);
+            if (movement > startPosition.z && movement < startPosition.z + Z_Boundary)
+                transform.position += new Vector3(0, 0, movement - transform.position.z);
+        }
 
-        movement *= Time.deltaTime * speed;
-        movement += transform.position.z;
+        float rotate=0;
+        if (Input.mousePosition.x < screenMoveSize)
+            rotate = -1 * Mathf.InverseLerp(screenMoveSize, 0, Input.mousePosition.x);
+        else if (Input.mousePosition.x > Screen.width - screenMoveSize)
+            rotate = 1 * Mathf.InverseLerp(Screen.width - screenMoveSize, Screen.width, Input.mousePosition.x);
 
-		if(movement>startPosition.z&&movement<startPosition.z+Z_Boundary)
-			transform.position += new Vector3 (0,0,movement-transform.position.z);
+        if (Input.GetKey(KeyCode.Q))
+            rotate = -1;
+        else if (Input.GetKey(KeyCode.E))
+            rotate = 1;
+
+        if (rotate != 0)
+        {
+            rotate *= Time.deltaTime * rot_speed;
+            rotate += transform.rotation.eulerAngles.y;
+            Debug.Log(rotate);
+            if (rotate< Y_Rot_Max&&rotate>Y_Rot_Min)
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, rotate, transform.rotation.eulerAngles.z);
+        }
 	}
 }
