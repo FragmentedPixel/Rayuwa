85a9ad23ce76d72fb76dfa990c15eb6f899ecd9c
VladDuncea <vlad_duncea_31@yahoo.com>
2017-05-22 23:40:49 +0300

    Camera Update

--- Assets/Generals/Scripts/CenterCameraController.cs
+++ Assets/Generals/Scripts/CenterCameraController.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CenterCameraController : MonoBehaviour {
-	public float speed = 10.0F;
-	private Vector3 startPosition;
-	public float Z_Boundary=50f;
-    [Range(0,0.5f)]
-    public float screenPercent = 0.1f;
+    private Vector3 startPosition;
     private float screenMoveSize;
-	void Start () 
+    [Header("Speeds")]
+    public float speed = 10.0F;
+
+    [Header("Boundaries")]
+    [Range(0, 0.5f)]
+    public float screenPercent = 0.0f;
+    public float Z_Boundary = 50f;
+    public float lookAtOffset = 25f;
+
+    [Header("LookAt")]
+    public Transform lookAt;
+
+    void Start () 
 	{
         screenPercent = PlayerPrefsManager.GetScrollBoundray();
 		startPosition = transform.position;
@@ -20,18 +28,23 @@ public class CenterCameraController : MonoBehaviour {
 
 	void Update() 
 	{
-		float movement = Input.GetAxis("Horizontal");
+		float movement = Input.GetAxis("Vertical");
 
-        if (movement == 0)
-            if (Input.mousePosition.x < screenMoveSize)
-                movement = -1 * Mathf.InverseLerp(screenMoveSize, 0, Input.mousePosition.x);
-            else if (Input.mousePosition.x > Screen.width - screenMoveSize)
-                movement = 1 * Mathf.InverseLerp(Screen.width - screenMoveSize, Screen.width, Input.mousePosition.x);
-
+       /*
+        if (Input.mousePosition.x < screenMoveSize)
+            movement = -1 * Mathf.InverseLerp(screenMoveSize, 0, Input.mousePosition.x);
+        else if (Input.mousePosition.x > Screen.width - screenMoveSize)
+            movement = 1 * Mathf.InverseLerp(Screen.width - screenMoveSize, Screen.width, Input.mousePosition.x);
+            */
         movement *= Time.deltaTime * speed;
         movement += transform.position.z;
 
-		if(movement>startPosition.z&&movement<startPosition.z+Z_Boundary)
-			transform.position += new Vector3 (0,0,movement-transform.position.z);
+        if (movement > startPosition.z && movement < startPosition.z + Z_Boundary)
+        {
+            transform.position += new Vector3(0, 0, movement - transform.position.z);
+            transform.LookAt(lookAt);
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x+lookAtOffset,transform.rotation.eulerAngles.y,transform.rotation.eulerAngles.z);
+            
+        }
 	}
 }
