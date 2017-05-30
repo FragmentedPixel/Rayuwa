598485c1df34d23c49a6604f296c90aae3d93fe2
Oprea Alexandru <oalexprea@yahoo.com>
2017-05-30 18:30:53 +0300

    Clean Up

---
+++ Assets/Cinematics/Scripts/CinematicaCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CinematicCamera : MonoBehaviour
+{
+    public CameraFrame[] frames;
+
+    private void Start()
+    {
+        StartCoroutine(CameraMovementCR());
+    }
+
+    private IEnumerator CameraMovementCR()
+    {
+        foreach (CameraFrame frame in frames)
+        {
+            float currentTime = 0f;
+
+            while (currentTime < frame.duration)
+            {
+                transform.position = Vector3.Lerp(frame.start.position, frame.final.position, currentTime / frame.duration);
+                transform.LookAt(frame.target);
+
+                currentTime += Time.deltaTime;
+                yield return null;
+            }
+        }
+    }
+}
+
+[Serializable]
+public class CameraFrame
+{
+    public Transform start;
+    public Transform final;
+    public Transform target;
+    public float duration;
+}
