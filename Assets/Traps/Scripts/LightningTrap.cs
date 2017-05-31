74a402598bb898cb22cd912187fa9e863722c751
Oprea Alexandru <oalexprea@yahoo.com>
2017-05-31 20:24:57 +0300

    Lightning Particules System

---
+++ Assets/Traps/Scripts/LightningTrap.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningTrap : Trap
+{
+    public ParticleSystem lightningParticules;
+    
+    public void SpawnLightning()
+    {
+        Instantiate(lightningParticules, transform.position, transform.rotation);
+    }
+
+}
