aa4b051623516f7bc7b57f65cbe2538eaee8fa84
Oprea Alexandru <oalexprea@yahoo.com>
2017-05-31 21:42:03 +0300

    Lightning Trap prefab

--- Assets/Traps/Scripts/LightningTrap.cs
+++ Assets/Traps/Scripts/LightningTrap.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 public class LightningTrap : Trap
 {
     public ParticleSystem lightningParticules;
-    
+    private bool triggered;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!triggered)
+        {
+            SpawnLightning();
+            triggered = true;
+            Destroy(gameObject, 3f);
+        }
+
+        UnitHealth unit = other.GetComponent<UnitHealth>();
+        if (unit != null)
+            unit.Die();
+    }
+
     public void SpawnLightning()
     {
-        Instantiate(lightningParticules, transform.position, transform.rotation);
+        Instantiate(lightningParticules, transform.position, transform.rotation, transform);
     }
 
 }
