9a6b12d880f59acf23bbbe8965842e5650ed325d
Oprea Alexandru <oalexprea@yahoo.com>
2017-06-02 19:51:16 +0300

    Aruncatori copaci part1

--- Assets/Enemys/Scripts/EnemyTreeThrower.cs
+++ Assets/Enemys/Scripts/EnemyTreeThrower.cs
@@ -6,18 +6,65 @@ using UnityEngine;
 [RequireComponent(typeof(Agent))]
 public class EnemyTreeThrower : MonoBehaviour
 {
+    #region Variabiles
     public GameObject treeHolded;
+    public float pickUpRange;
+    public float attackSpeed;
 
-    private GameObject targetTree;
+    private float waitTime;
+    private GameObject treeTargeted;
     private Agent agent;
+    private bool isTimeToAttack;
+    #endregion
 
+    #region Initialization
     private void Start()
     {
         agent = GetComponent<Agent>();
     }
+    #endregion
 
-    
+    #region Update
+    private void Update()
+    {
+        if (treeHolded != null)
+        {
+            if (waitTime >= attackSpeed)
+                Throw();
+            else
+                Wait();
+        }
+        else if (treeTargeted != null)
+        {
+            if (DistanceToObject(transform, treeTargeted.transform) < pickUpRange)
+                PickUp();
+            else
+                GoTowardsTargeted();
+        }
+        else
+            FindClosestTree();
+    }
+    #endregion
 
+    #region Methods
+    private void Throw()
+    {
+        Debug.Log("Inceput animatie aruncat copaci.");
+        Destroy(treeHolded);
+        waitTime = 0f;
+    }
+    private void Wait()
+    {
+        waitTime += Time.deltaTime;
+    }
+    private void PickUp()
+    {
+        treeHolded = treeTargeted;
+    }
+    private void GoTowardsTargeted()
+    {
+        agent.MoveToDestination(treeTargeted.transform.position);
+    }
     private void FindClosestTree()
     {
         GameObject[] trees = GameObject.FindGameObjectsWithTag("Tree");
@@ -27,10 +74,12 @@ public class EnemyTreeThrower : MonoBehaviour
         {
             float newDistance = DistanceToObject(transform, tree.transform);
             if (newDistance < distance)
-                targetTree = tree;
+                treeTargeted = tree;
         }
     }
+    #endregion
 
+    #region Utility
     private float DistanceToObject(Transform t1, Transform t2)
     {
         Vector3 startPos = new Vector3(t1.position.x, 0f, t1.position.z);
@@ -38,4 +87,5 @@ public class EnemyTreeThrower : MonoBehaviour
 
         return Vector3.Distance(startPos, finalPos);
     }
+    #endregion
 }
