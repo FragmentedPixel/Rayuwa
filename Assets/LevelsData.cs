0c0c80959931451f566bf9b52c8172ecec4fbca7
Oprea Alexandru <oalexprea@yahoo.com>
2017-06-06 18:38:03 +0300

    LevelsUI

---
+++ Assets/LevelsData.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class LevelsData : MonoBehaviour
+{
+    public static LevelsData instance;
+
+    public bool[] levels;
+
+    #region Serialization
+    private string fileLocation;
+    private void Start()
+    {
+        instance = this;
+        DontDestroyOnLoad(this);
+        fileLocation = Application.persistentDataPath + "/levels.data";
+        Load();
+    }
+    private void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = new FileStream(fileLocation, FileMode.Create, FileAccess.Write, FileShare.Write);
+
+        Levels progress = new Levels(levels);
+
+        bf.Serialize(file, progress);
+        file.Close();
+    }
+    private void Load()
+    {
+        if (!File.Exists(fileLocation))
+            return;
+
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = new FileStream(fileLocation, FileMode.Open, FileAccess.Read, FileShare.Read);
+
+        Levels progress = bf.Deserialize(file) as Levels;
+
+        levels = progress.levels;
+    }
+    private void OnDestroy()
+    {
+        Save();
+    }
+    #endregion
+}
+
+[Serializable]
+public class Levels
+{
+    public bool[] levels;
+
+    public Levels(bool[] _levels)
+    {
+        levels = _levels;
+    }
+}
---
+++ Assets/UI/Scripts/LevelPanelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPanelManager : MonoBehaviour
+{
+    public GameObject levelImage;
+
+    private void Start()
+    {
+        PopulatePanel();
+    }
+
+    private void PopulatePanel()
+    {
+        int count = LevelsData.instance.levels.Length;
+    }
+
+}
