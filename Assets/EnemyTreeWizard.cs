11996749ec45f61767e6fa4c8f1a1aa1b952a185
Oprea Alexandru <oalexprea@yahoo.com>
2017-06-11 15:05:51 +0300

    Reload Points fixed + Tree Wizard

---
+++ Assets/EnemyTreeWizard.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class EnemyTreeWizard : MonoBehaviour
+{
+    public float range;
+    public float damage;
+    public float castTime = 0f;
+    public Image castBar;
+    
+    private float currentCastTime = 0f;
+    private TreeLauncher treeLauncher;
+    private List<GameObject> allTrees;
+    private Grid grid;
+
+    private void Start()
+    {
+        treeLauncher = GetComponent<TreeLauncher>();
+        grid = FindObjectOfType<Grid>();
+        allTrees = GameObject.FindGameObjectsWithTag("Tree").ToList();
+    }
+
+    private void Update()
+    {
+        if (currentCastTime < castTime)
+            currentCastTime += Time.deltaTime;
+
+        else
+            Throw();
+
+        UpdateCastBar();
+    }
+
+    private void Throw()
+    {
+        Transform closestTree = allTrees[0].transform;
+        allTrees.Remove(allTrees[0]);
+        Transform target = FindObjectOfType<UnitController>().transform;
+
+        closestTree.gameObject.layer = 11;
+        EnemyTree enemyTree = closestTree.gameObject.AddComponent<EnemyTree>();
+        enemyTree.damage = damage;
+
+        treeLauncher.Launch(closestTree, target);
+        currentCastTime = 0f;
+        grid.ReCalculateGird();
+    }
+
+    private void UpdateCastBar()
+    {
+        castBar.fillAmount = currentCastTime / castTime;
+    }
+}
