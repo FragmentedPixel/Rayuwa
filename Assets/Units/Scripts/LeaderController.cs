8cc7683c33fca823697e3c017bed5f0bc0466817
Oprea Alexandru <oalexprea@yahoo.com>
2017-06-14 00:21:23 +0300

    Walking + Idle animations
    Ne trebuie GO cu animatorul activ si sa fie acelasi la amandoua.

--- Assets/Units/Scripts/LeaderController.cs
+++ Assets/Units/Scripts/LeaderController.cs
@@ -9,62 +9,78 @@ public class LeaderController : MonoBehaviour {
     public float cameraVerticalSpeed = 1f;
 
     public UnitController unit;
+    public Animator anim;
+    private Rigidbody rb;
 
     void Start ()
     {
-
+        //anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && (unit.fightState.lastAttack + unit.fightSpeed < Time.time))
         {
-            if (unit.fightState.lastAttack + unit.fightSpeed < Time.time)
+            unit.fightState.lastAttack = Time.time;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
             {
-                unit.fightState.lastAttack = Time.time;
+                EnemyHealth enemy = hit.transform.GetComponent<EnemyHealth>();
 
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
+                if (enemy != null)
+                    unit.target = enemy.transform;
+            }
 
-                if (Physics.Raycast(ray, out hit))
-                {
-                    EnemyHealth enemy = hit.transform.GetComponent<EnemyHealth>();
-
-                    if (enemy != null)
-                        unit.target = enemy.transform;
-                }
-
-                if (unit.target != null)
-                {
-                    //TODO: Event missing from animation.
-                    GetComponent<Animator>().SetTrigger("MeleeAttack");
-                    (unit as MeleeUnitController).SwordHit();
-                }
+            if (unit.target != null)
+            {
+                //TODO: Event missing from animation.
+                anim.SetTrigger("MeleeAttack");
+                (unit as MeleeUnitController).SwordHit();
+                
             }
         }
     }
 
-    // Update is called once upon a frame
-    void FixedUpdate ()
+    private void FixedUpdate ()
     {
+        Movement();
+        CharacterRotation();
+        CameraRotation();
+    }
 
+    private void Movement()
+    {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        GetComponent<Rigidbody>().velocity =transform.TransformVector(new Vector3(horizontal, 0, vertical).normalized * Time.fixedDeltaTime * movementSpeed);
 
-        //Rotates Player on "X" Axis Acording to Mouse Input
-        float h = cameraHorizontalSpeed * Input.GetAxis("Mouse X");
-        transform.Rotate(0, h, 0);
+        anim.SetBool("Walking", (horizontal + vertical != 0));
 
+        rb.velocity = transform.TransformVector(new Vector3(horizontal, 0, vertical).normalized * Time.fixedDeltaTime * movementSpeed);
+    }
+
+    private void CameraRotation()
+    {
         //Rotates Player on "Y" Axis Acording to Mouse Input
         float v = cameraVerticalSpeed * Input.GetAxis("Mouse Y");
         Camera.main.transform.Rotate(-v, 0, 0);
 
         Vector3 rotation = Camera.main.transform.localEulerAngles;
-        if (rotation.x > 30&& rotation.x<200)
+        if (rotation.x > 30 && rotation.x < 200)
             rotation.x = 30;
-        else if (rotation.x < 330&&rotation.x>200)
+        else if (rotation.x < 330 && rotation.x > 200)
             rotation.x = 330;
-        Camera.main.transform.localEulerAngles =rotation;
+        Camera.main.transform.localEulerAngles = rotation;
+    }
+
+    private void CharacterRotation()
+    {
+
+        //Rotates Player on "X" Axis Acording to Mouse Input
+        float h = cameraHorizontalSpeed * Input.GetAxis("Mouse X");
+        transform.Rotate(0, h, 0);
     }
 }
