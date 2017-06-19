fc01f80df0c04317188209e50fafaf59f14148d2
Oprea Alexandru <oalexprea@yahoo.com>
2017-06-20 00:24:05 +0300

    Enemy Tree inside Collider

--- Assets/Enemys/Scripts/EnemyTree.cs
+++ Assets/Enemys/Scripts/EnemyTree.cs
@@ -5,19 +5,38 @@ using UnityEngine;
 public class EnemyTree : MonoBehaviour
 {
     public float damage;
+    private List<UnitHealth> units;
+
+    private void Start()
+    {
+        units = new List<UnitHealth>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        UnitHealth unitHealth = other.GetComponent<UnitHealth>();
-
-        if (unitHealth != null)
-            unitHealth.Hit(damage, null);
+        UnitHealth unit = other.GetComponent<UnitHealth>();
+        if (unit != null && !units.Contains(unit))
+            units.Add(unit);
 
         if (other.transform.name == "DangerZone(Clone)")
         {
+            DealDamage(damage);
             Destroy(gameObject, 3f);
             Destroy(other.transform.gameObject);
             Destroy(this);
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        UnitHealth unit = other.GetComponent<UnitHealth>();
+        if (unit != null && units.Contains(unit))
+            units.Remove(unit);
+    }
+
+    public void DealDamage(float damage)
+    {
+        foreach (UnitHealth unit in units)
+            unit.Hit(damage);
     }
 }
