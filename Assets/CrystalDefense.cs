ba9df0a52a5f0a194478774045564ddd735343e4
Oprea Alexandru <oalexprea@yahoo.com>
2017-06-20 17:12:52 +0300

    Collapse on Crystal

---
+++ Assets/CrystalDefense.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalDefense : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        UnitHealth unit = other.GetComponent<UnitHealth>();
+
+        if (unit == null)
+            return;
+
+        //Aggro every enemy.
+        /*
+        EnemyHealth[] enemies = FindObjectsOfType<EnemyHealth>();
+        foreach (EnemyHealth enemy in enemies)
+            enemy.Hit(0, unit);
+        */
+    }
+
+}
